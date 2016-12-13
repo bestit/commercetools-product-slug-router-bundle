a295a6c93a20eab8dc95417a799fe6ed5e56200d
Bjoern Lange <lange@bestit-online.de>
2016-12-13 21:59:06 +0100

    Made the values of the routing process configurable

--- src/Router/ProductRouter.php
+++ src/Router/ProductRouter.php
@@ -19,18 +19,110 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 class ProductRouter implements RouterInterface
 {
     /**
+     * The default controller.
+     * @var string
+     */
+    const DEFAULT_CONTROLLER = 'BestIt\Frontend\ProductBundle\Controller\DetailController::indexAction';
+
+    /**
+     * The default route.
+     * @var string
+     */
+    const DEFAULT_ROUTE = 'best_it_frontend_product_detail_index';
+
+    /**
+     * The logical/full name for the used controller.
+     * @var string
+     */
+    private $controller = '';
+
+    /**
+     * The repository to fetch products by slug.
      * @var ProductRepositoryInterface
      */
     private $repository;
 
     /**
-     * ProductRouter constructor.
-     *
-     * @param ProductRepositoryInterface $repository
+     * The used route name for this router.
+     * @var string
      */
-    public function __construct(ProductRepositoryInterface $repository)
+    private $route = '';
+
+    /**
+     * ProductRouter constructor.
+     * @param ProductRepositoryInterface $repository
+     * @param string $controller
+     * @param string $route
+     */
+    public function __construct(
+        ProductRepositoryInterface $repository,
+        string $controller = self::DEFAULT_CONTROLLER,
+        $route = self::DEFAULT_ROUTE
+    ) {
+        $this
+            ->setController($controller)
+            ->setRepository($repository)
+            ->setRoute($route);
+    }
+
+    /**
+     * Returns the logical/full name for the used controller.
+     * @return string
+     */
+    private function getController(): string
+    {
+        return $this->controller;
+    }
+
+    /**
+     * Returns the repository to fetch products by slug.
+     * @return ProductRepositoryInterface
+     */
+    private function getRepository(): ProductRepositoryInterface
+    {
+        return $this->repository;
+    }
+
+    /**
+     * Returns the used route name for this router.
+     * @return string
+     */
+    private function getRoute(): string
+    {
+        return $this->route;
+    }
+
+    /**
+     * Sets the logical/full name for the used controller.
+     * @param string $controller
+     * @return ProductRouter
+     */
+    private function setController(string $controller): ProductRouter
+    {
+        $this->controller = $controller;
+        return $this;
+    }
+
+    /**
+     * Sets the repository to fetch products by slug.
+     * @param ProductRepositoryInterface $repository
+     * @return ProductRouter
+     */
+    private function setRepository(ProductRepositoryInterface $repository): ProductRouter
     {
         $this->repository = $repository;
+        return $this;
+    }
+
+    /**
+     * Sets the used route name for this router.
+     * @param string $route
+     * @return ProductRouter
+     */
+    private function setRoute(string $route): ProductRouter
+    {
+        $this->route = $route;
+        return $this;
     }
 
     /**
@@ -49,7 +141,8 @@ class ProductRouter implements RouterInterface
         $name,
         $parameters = array(),
         $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH
-    ) : string {
+    ) : string
+    {
         throw new RouteNotFoundException('Not supported by ProductRouter');
     }
 
@@ -59,14 +152,14 @@ class ProductRouter implements RouterInterface
     public function match($pathinfo) : array
     {
         try {
-            $product = $this->repository->getProductBySlug(trim($pathinfo, '/'));
+            $product = $this->getRepository()->getProductBySlug(trim($pathinfo, '/'));
         } catch (ProductNotFoundException $e) {
             throw new ResourceNotFoundException('Not product found for ProductRouter');
         }
 
         return [
-            '_controller' => 'BestIt\Frontend\ProductBundle\Controller\DetailController::indexAction',
-            '_route' => 'best_it_frontend_product_detail_index',
+            '_controller' => $this->getController(),
+            '_route' => $this->getRoute(),
             'product' => $product,
         ];
     }
